7b22c64aa7c1f0b2b58b63dc5b45951a3c7d1fc2
jkroso <jkroso@gmail.com>
2013-03-16 23:41:55 +1300

    make example clearer

--- examples/basic.js
+++ examples/basic.js
@@ -1,13 +1,13 @@
 
 var graph = require('../src')
 
-var inc = graph({
-	b: function(a){
+var ib = graph({
+	inc: function(a){
 		return a + 1
 	},
-	c: function(b){
-		return b + '!'
+	bang: function(inc){
+		return inc + '!'
 	}
 })
 
-console.log(inc(1))
+console.log(ib(1))
